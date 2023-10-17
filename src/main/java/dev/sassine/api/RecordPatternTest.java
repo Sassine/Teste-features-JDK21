678d213c26bb04bac2ee5bce524a77f2b454031e
Sassine El-Asmar <sassineasmar@gmail.com>
2023-10-17 20:02:41 -0300

    Add: formato ideial como exemplo de utilizar null e default no Switch Case

--- src/main/java/dev/sassine/api/RecordPatternTest.java
+++ src/main/java/dev/sassine/api/RecordPatternTest.java
@@ -3,45 +3,44 @@ package dev.sassine.api;
 import static java.lang.System.out;
 
 class RecordPatternTest {
-	
-	public void init() {
-		var p = new Pessoa("Sassine", 26);
-		validaSeDeMaiorSwitchJDK17(p);
-		validaSeDeMaiorSwitchJDK21(p);
-		validaSeDeMaiorJDK17(p);
-		validaSeDeMaiorJDK21(p);
-	}
 
-	static void validaSeDeMaiorSwitchJDK17(Object p) {
-		switch (p) {
-		case Pessoa p1 -> out.println("%s Permitido".formatted(p1.nome()));
-		default -> out.println("Não permitido");
-		}
+    public void init() {
+        var p = new Pessoa("Sassine", 26);
+        validaSeDeMaiorSwitchJDK17(p);
+        validaSeDeMaiorSwitchJDK21(p);
+        validaSeDeMaiorJDK17(p);
+        validaSeDeMaiorJDK21(p);
+    }
 
-	}
+    static void validaSeDeMaiorSwitchJDK17(Object p) {
+        switch (p) {
+            case Pessoa p1 -> out.println("%s Permitido".formatted(p1.nome()));
+            case null, default -> out.println("Não permitido");
+        }
+    }
 
-	static void validaSeDeMaiorSwitchJDK21(Object p) {
-		switch (p) {
-		case Pessoa(var nome, var idade) when idade >= 18 -> out.println("%s Permitido".formatted(nome));
-		case null -> out.println("Null pointer não");
-		default -> out.println("Não permitido");
-		}
-	}
+    static void validaSeDeMaiorSwitchJDK21(Object p) {
+        switch (p) {
+            case Pessoa(var nome, var idade) when idade >= 18 -> out.println("%s Permitido".formatted(nome));
+            case null -> out.println("Null pointer não");
+            default -> out.println("Não permitido");
+        }
+    }
 
-	static void validaSeDeMaiorJDK17(Object p) {
-		if (p instanceof Pessoa p1 && p1.idade() >= 18) {
-			out.println("%s Permitido".formatted(p1.nome()));
-		} else {
-			out.println("Não permitido");
-		}
-	}
+    static void validaSeDeMaiorJDK17(Object p) {
+        if (p instanceof Pessoa p1 && p1.idade() >= 18) {
+            out.println("%s Permitido".formatted(p1.nome()));
+        } else {
+            out.println("Não permitido");
+        }
+    }
 
-	static void validaSeDeMaiorJDK21(Object p) {
-		if (p instanceof Pessoa(var nome, var idade) && idade >= 18) {
-			out.println("%s Permitido".formatted(nome));
-		} else {
-			out.println("Não permitido");
-		}
-	}
+    static void validaSeDeMaiorJDK21(Object p) {
+        if (p instanceof Pessoa(var nome, var idade) && idade >= 18) {
+            out.println("%s Permitido".formatted(nome));
+        } else {
+            out.println("Não permitido");
+        }
+    }
 
 }
